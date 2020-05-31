22d539c98ac26825cd26dd43409eb4cd4f57c934
Glitch (neura) <none>
2020-05-31 08:43:10 +0000

    Move from Python backend

--- src/core/web.ts
+++ src/core/web.ts
@@ -4,6 +4,12 @@ import log from '../log'
 export const app = express()
 export const shouldStart = process.env.ENABLE_WEB === 'true'
 
+app.get('/', (_, res) => res.json(process.uptime()))
+
+app.get('/invite', (_, res) => {
+  res.redirect('https://discord.com/oauth2/authorize?client_id=704395625332211762&permissions=8&scope=bot')
+})
+
 if (shouldStart) {
   app.listen(process.env.PORT, () => {
     log.info('Started web listening on port ' + process.env.PORT, { service: 'web' })
